16d9c4a62d8fbc0f6e9245aef0a9b2e393b0626c
Addisalem Abera <36120997+ADIETH@users.noreply.github.com>
2018-10-06 22:38:46 -0500

    finishing my homwork late

---
+++ PyBank/main.py
@@ -0,0 +1,68 @@
+# First part should be importing the OS module to create mechanism to create file path across operating system
+import os
+
+# call module to read CSv files
+import csv
+
+# provide location to load the source data for analysis and to store the analysis report
+File_to_Load = os.path.join('Resources', 'budget_data.csv')
+Out_put_file = os.path.join('Report', 'budget_repot.txt')
+
+# Setting parameters for the Analysis
+Total_Months = 0
+previous_Revenue = 0
+Month_change = []
+Revenue_Change_List = []
+Greatest_Increase = ["", 0]
+Greatest_Decrease = ["", 9999999999999999999]
+Total_Revenue = 0
+
+# extract the data from CSV file and assign it into a list of Dictionaries
+with open(File_to_Load) as budget_data:
+    reader = csv.DictReader(budget_data)
+
+    for row in reader:
+
+        # Trace Total months and Revenue
+        Total_Months = Total_Months + 1
+        Total_Revenue = Total_Revenue + int(row["Profit/Losses"])
+
+        # Trace the changes for revenue and duration
+        Revenue_Change = int(row["Profit/Losses"]) - previous_Revenue
+        previous_Revenue = int(row["Profit/Losses"])
+        Revenue_Change_List = Revenue_Change_List + [Revenue_Change]
+        Month_change = Month_change + [row["Date"]]
+
+        # Determine Greatest increase and decrease in profit/losses  over the entire period
+        if (Revenue_Change > Greatest_Increase[1]):
+            Greatest_Increase[0] = row["Date"]
+            Greatest_Increase[1] = Revenue_Change
+
+        if (Revenue_Change < Greatest_Decrease[1]):
+            Greatest_Decrease[0] = row["Date"]
+            Greatest_Decrease[1] = Revenue_Change
+
+        # Trace Average Revenue Change
+        Average_Revenue = sum(Revenue_Change_List) / len(Revenue_Change_List)
+
+        # Generate Report 
+        output = (
+    f"\nFinancial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {Total_Months}\n"
+    f"Total Revenue: ${Total_Revenue}\n"
+    f"Average Change: ${Average_Revenue}\n"
+    f"Greatest Increase in Revenue: {Greatest_Increase[0]} (${Greatest_Increase[1]})\n"
+    f"Greatest Decrease in Revenue: {Greatest_Decrease[0]} (${Greatest_Decrease[1]})\n")
+
+# Print the output (to terminal)
+print(output)
+
+# Export the results to text file
+with open(Out_put_file, "w") as txt_file:
+    txt_file.write(output)
+
+
+
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,67 @@
+# First part should be importing the OS module to create mechanism to create file path across op
+import os 
+
+# call module to read CSv files
+import csv
+
+# provide location to load the source data for analysis and to store the analysis report
+Pypoll = os.path.join('Resources', 'election_data.csv')
+Out_put_file = os.path.join('Report', 'election_results.txt')
+
+
+# create list to store data
+Count = 0
+Candidate_List = []
+Unique_Candidate = []
+Vote_Count = []
+Vote_Percent = []
+
+# extract the data from CSV file and assign it into a list of Dictionaries
+with open(Pypoll, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader) 
+
+    # Conduct the count 
+    for row in csvreader:
+        # count the total votes
+        Count = Count + 1
+        # Add candidate name to candidate name list
+        Candidate_List.append(row[2])
+        # create a set from the candidate list to get the unique candidate name
+    for A in set(Candidate_List):
+        Unique_Candidate.append(A)
+        # B represent total number of votes for each candidate
+        B = Candidate_List.count(A)
+        Vote_Count.append(B)
+        # C represent percent of total votes for each candidate
+        C = (B/Count)*100
+        Vote_Percent.append(C)
+        # Calculate winner and winning vote
+        Winning_Vote_Count = max(Vote_Count)
+        Winner = Unique_Candidate[Vote_Count.index(Winning_Vote_Count)]
+
+
+print("------------------------------")
+print("Election Results")
+print("------------------------------") 
+print("Total Votes:" + str(Count))
+print("------------------------------")
+for i in range(len(Unique_Candidate)):
+            print(Unique_Candidate[i] + ": " + str(Vote_Percent[i]) + "% (" + str(Vote_Count[i])+ ")")
+print("-----------------------------")
+print("Winner : " + Winner)
+print("-----------------------------")   
+
+
+#  Open the result file
+
+with open(Out_put_file, 'w') as text:
+    text.write("Election Results\n")
+    text.write("---------------------------------------\n")
+    text.write("Total Vote: " + str(Count) + "\n")
+    text.write("---------------------------------------\n")
+    for i in range(len(set(Unique_Candidate))):
+        text.write(Unique_Candidate[i] + ": " + str(Vote_Percent[i]) +"% (" + str(Vote_Count[i]) + ")\n")
+    text.write("---------------------------------------\n")
+    text.write(" winner : " + Winner + "\n")
+    text.write("---------------------------------------\n")
